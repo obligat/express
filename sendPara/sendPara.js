9602dba517e7bf020e1b1005d5772f51950a41a8
dujinqiao <duzi1996@qq.com>
2016-08-05 00:33:26 +0800

    add a demo to get param by body

--- sendPara/sendPara.js
+++ sendPara/sendPara.js
@@ -9,6 +9,10 @@ sendParam.get('/user', function (req, res) {
     res.send(req.query.name);
 });
 
+sendParam.post('/user', function (req, res) {
+    res.send(req.body.name);
+});
+
 
 sendParam.listen(3000, function () {
     console.log('3000 port ');
--- spec/sendPara-spec.js
+++ spec/sendPara-spec.js
@@ -12,3 +12,20 @@ describe('GET /user', function () {
             })
     })
 });
+
+describe('POST /user', function () {
+    it("should get a parameter by body", function (done) {
+        request .post('/user')
+            .send({name: "eng"})
+            .expect('eng')
+            .end(function (err, res) {
+                if(err){
+                    done.fail(err);
+                }
+                else {
+                    done();
+                }
+            });
+    })
+});
+
